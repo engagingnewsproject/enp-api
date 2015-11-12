67477ddebbd0b0f02576a366f114d4c29dd0bbda
Jerry <jones.jeremydavid@gmail.com>
2015-11-12 10:14:15 -0600

    added log file and moved the save process to a guid file name

--- api.php
+++ api.php
@@ -1,36 +1,48 @@
 <?php
 
-
-// require database connection
-require 'database/db_functions.php';
-
-require 'Enp_API_Response_Class.php';
-require 'Enp_API_Save_Class.php';
-
-
-$sentJSON = file_get_contents('php://input');
-$data= json_decode( $sentJSON, TRUE ); //convert JSON into array*/
-
-
 /*
 // TESTING
 $sent = array(
     "site_url"=> "http://dev/respect",
     "meta_id"=>  "13212",
     "button"=>  "respect",
-    "clicks"=>  "99",
+    "clicks"=>  "100",
     "post_id"=>  "4",
     "comment_id"=> 0,
     "post_type"=>  "post",
     "button_url"=> "http://dev/respect/?p=4"
 );
 $sentJSON = json_encode($sent);
-$data= json_decode( $sentJSON, TRUE ); //convert JSON into array*/
+//$data= json_decode( $sentJSON, TRUE ); //convert JSON into array*/
+
+// live from plugin data
+$sentJSON = file_get_contents('php://input');
+
+// get the current server URL, then go back to the root and to our processing file
+$url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+$url = $url.'/../process_a7d5f2d8b52c444aac1ef79b1f0140d1.php';
+
+// pass connection to our guid processing file to make it harder to track the connection
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $sentJSON);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($sentJSON))
+);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
 
 
-// Process the save
-$apiSave = new Enp_API_Save($data);
-// Send the Save Response back to the requested site
-$apiResponse = new Enp_API_Response($apiSave->response);
+$log_result = $result."\n";
+// Write the contents to the file,
+// using the FILE_APPEND flag to append the content to the end of the file
+// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+file_put_contents('log-e104c5b7-5ba5-4e14-94fe-0d31c6ccf0e1.txt', $log_result, FILE_APPEND | LOCK_EX);
+
 
 ?>
---
+++ process_a7d5f2d8b52c444aac1ef79b1f0140d1.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// require database connection
+require 'database/db_functions.php';
+
+require 'Enp_API_Response_Class.php';
+require 'Enp_API_Save_Class.php';
+
+
+$sentJSON = file_get_contents('php://input');
+$data= json_decode( $sentJSON, TRUE ); //convert JSON into array*/
+
+
+
+// Process the save
+$apiSave = new Enp_API_Save($data);
+// Send the Save Response back to the requested site
+$apiResponse = new Enp_API_Response($apiSave->response);
+
+?>
