3393eb03069021df376da30b936c6978bd20542d
Jerry <jones.jeremydavid@gmail.com>
2015-11-16 18:04:04 -0600

    added error log and put process to sleep for 2 seconds on error

--- Enp_API_Response_Class.php
+++ Enp_API_Response_Class.php
@@ -47,6 +47,7 @@ class Enp_API_Response {
             'success' => array('HTTP Response' => 200, 'Message' => 'Success'),
             'insert-success' => array('HTTP Response' => 200, 'Message' => 'Insert Success'),
             'update-success' => array('HTTP Response' => 200, 'Message' => 'Update Success'),
+            'logged-error' => array('HTTP Response' => 200, 'Message' => 'Logged Error'),
             'no-changes' => array('HTTP Response' => 200, 'Message' => 'No Changes'),
             'uanuthorized' => array('HTTP Response' => 401, 'Message' => 'Unauthorized'),
             'invalid-request' => array('HTTP Response' => 404, 'Message' => 'Invalid Request'),
--- api.php
+++ api.php
@@ -17,26 +17,40 @@ $sentJSON = json_encode($sent);
 
 // require database connection
 require 'database/db_functions-004cf329-659d-40d4-9c7e-6c2dea495653.php';
-
-require 'Enp_API_Response_Class.php';
 require 'Enp_API_Save_Class.php';
 
 $sentJSON = file_get_contents('php://input');
 $data= json_decode( $sentJSON, TRUE ); //convert JSON into array*/
-// append the timestamp
-$data['updated'] = date("Y-m-d H:i:s");
 
-// Process the save
-$apiSave = new Enp_API_Save($data);
+if(!empty($data['error'])) {
+    // we have an error, so log it.
+    $log_result = 'Error: ';
+    foreach($data as $key=>$value) {
+        $log_result .= $key.'['.$value.'],';
+    }
+    // pop the last comma
+    $log_result = substr($log_result, 0, -1);
+    $log_result .= "\n";
+    // Write the contents to the file,
+    file_put_contents('error-log/errors.txt', $log_result, FILE_APPEND | LOCK_EX);
+    // go to sleep for 2 seconds
+    usleep(2000000);
+    die();
+} else {
+    require 'Enp_API_Response_Class.php';
+    // append the timestamp
+    $data['updated'] = date("Y-m-d H:i:s");
+    // Process the save
+    $apiSave = new Enp_API_Save($data);
 
-$log_result = $apiSave->response."|".$data['site_url']."|".$data['meta_id']."|".$data['post_id']."|".$data['comment_id']."|".$data['updated']."\n";
-// Write the contents to the file,
-// using the FILE_APPEND flag to append the content to the end of the file
-// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-file_put_contents('log/log-e104c5b7-5ba5-4e14-94fe-0d31c6ccf0e1.txt', $log_result, FILE_APPEND | LOCK_EX);
+    $log_result = $apiSave->response."|".$data['site_url']."|".$data['meta_id']."|".$data['post_id']."|".$data['comment_id']."|".$data['updated']."\n";
+    // Write the contents to the file,
+    file_put_contents('log/log-e104c5b7-5ba5-4e14-94fe-0d31c6ccf0e1.txt', $log_result, FILE_APPEND | LOCK_EX);
 
 
-// Send the Save Response back to the requested site
-$apiResponse = new Enp_API_Response($apiSave->response);
+    // Send the Save Response back to the requested site
+    $apiResponse = new Enp_API_Response($apiSave->response);
+}
 
+exit();
 ?>
